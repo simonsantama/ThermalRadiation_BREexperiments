55667e7f7a514156e4e5f19247b7793834531202
SANTAMARIA GARCIA Simon <s1475174@ed.ac.uk>
2021-04-22 20:27:29 +0100

    Pep8 compliant

--- analysis/temperatures/0_upload_and_pickle.py
+++ analysis/temperatures/0_upload_and_pickle.py
@@ -1,7 +1,8 @@
 """
 This script uploads temperatures from each test.
-This data is taken from Alastair's summary file, and it has already been cleaned to a point.
-It then renames the columns to distribute them by thermocouple tree and stores all the raw data into one picke file.
+This data is taken from Alastair's summary file, and it has already been
+cleaned to a point. It then renames the columns to distribute them by
+thermocouple tree and stores all the raw data into one picke file.
 """
 
 import pandas as pd
@@ -19,32 +20,29 @@ for test_name in ["Alpha2", "Beta1", "Beta2", "Gamma"]:
     all_raw_data[test_name] = {}
     
     # upload the data from both data loggers
-    file_address = f"C:/Users/s1475174/Documents/Python_Projects/BRE_Paper_2016/unprocessed_data/temperature/{test_name}_GasPhaseTemperatures.xlsx"
-    
+    file_address = (r"C:/Users/s1475174/Documents/Python_Projects/T"
+                    r"hermalRadiation_BREexperiments/unprocessed_data/t"
+                    f"emperature/{test_name}_GasPhaseTemperatures.xlsx")
+
     # two data loggers means two different data frames
     df_A = pd.read_excel(file_address, sheet_name = "LoggerA")
     df_B = pd.read_excel(file_address, sheet_name = "LoggerB")
-    
+
     # parse the data
     for df in [df_A, df_B]:
         # convert time to seconds
-        df.loc[:, "testing_time"] = df.loc[:, "testing_time"]*60
-        
+        df.loc[:, "testing_time"] = df.loc[:, "testing_time"] * 60
+
         # iterate over all columns and apply meaningful names
         for column in df.columns[1:]:
             new_col_name = column.split("<")[1].split(">")[0]
             df.loc[:, new_col_name] = df.loc[:, column]
-            
+
     all_raw_data[test_name]["LoggerA"] = df_A
     all_raw_data[test_name]["LoggerB"] = df_B
     
     print(f" time taken: {np.round(time.time() - start,2)} seconds")
     
-file_address = "Temperatures_Raw.pkl"
+file_address = "temperatures_rawdata.pkl"
 with open(file_address, "wb") as handle:
     pickle.dump(all_raw_data, handle)
-        
-        
-        
-
-
